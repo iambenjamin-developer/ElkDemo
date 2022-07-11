1a9c93bb263478d563badba6e524099ef6637801
Benjamin Correa <benjamin.c@smartwaystudio.com>
2022-07-11 09:37:59 -0300

    User controller

---
+++ ElkDemo/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using ElkDemo.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ElkDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+
+        [HttpGet("{id}")]
+        public User Get(string id)
+        {
+            return new User();
+        }
+
+
+        [HttpPost]
+        public void Post([FromBody] User value)
+        {
+        }
+
+
+    }
+}
---
+++ ElkDemo/Entities/User.cs
@@ -0,0 +1,9 @@
+﻿namespace ElkDemo.Entities
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Age { get; set; }
+        public string Address { get; set; }
+    }
+}
